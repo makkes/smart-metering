676d4f61985a7720f82f47db9956fd6d822a4982
Max Jonas Werner <mail@makk.es>
2021-01-11 21:33:42 +0100

    feat: more generic runner
    The runner now walks through the 01-plot/ tree and calls every
    `plot.sh` script it can find.

--- run/go.mod
+++ run/go.mod
@@ -2,4 +2,4 @@ module github.com/makkes/smartmeter
 
 go 1.15
 
-require github.com/makkes/judo v0.0.0-20210107224728-da2d2520560d
+require github.com/makkes/judo v0.0.0-20210111202836-11654fe0cfb3
--- run/main.go
+++ run/main.go
@@ -3,58 +3,36 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"time"
 
 	"github.com/makkes/judo/process"
 )
 
-func plotValues() {
-	ticker := time.Tick(60 * time.Second)
-	for {
-		_, sigCh := process.Start("/usr/bin/gnuplot", []string{"plot.gnuplot"}, "../01-plot")
-		res := <-sigCh
-		if res.Err != nil {
-			fmt.Fprintf(os.Stderr, "error running gnuplot: %s\n", res.Err)
-		}
-		if res.ExitCode != 0 {
-			fmt.Fprintf(os.Stderr, "error running gnuplot: exit status %d\n", res.ExitCode)
-		}
-		<-ticker
+func plot(dir string) {
+	_, sigCh := process.Start("./plot.sh", nil, dir, os.Stdout)
+	res := <-sigCh
+	if res.Err != nil {
+		fmt.Fprintf(os.Stderr, "error plotting graph in %s: %s\n", dir, res.Err)
 	}
-}
-
-func plotDaily() {
-	ticker := time.Tick(1 * time.Hour)
-	for {
-		_, sigCh := process.Start("./plot.sh", nil, "../01-plot/daily")
-		res := <-sigCh
-		if res.Err != nil {
-			fmt.Fprintf(os.Stderr, "error plotting daily graph: %s\n", res.Err)
-		}
-		if res.ExitCode != 0 {
-			fmt.Fprintf(os.Stderr, "error plotting daily graph: exit status %d\n", res.ExitCode)
-		}
-		<-ticker
-	}
-}
-
-func plotHours() {
-	ticker := time.Tick(1 * time.Hour)
-	for {
-		_, sigCh := process.Start("./plot.sh", nil, "../01-plot/hours")
-		res := <-sigCh
-		if res.Err != nil {
-			fmt.Fprintf(os.Stderr, "error plotting hourly graph: %s\n", res.Err)
-		}
-		if res.ExitCode != 0 {
-			fmt.Fprintf(os.Stderr, "error plotting hourly graph: exit status %d\n", res.ExitCode)
-		}
-		<-ticker
+	if res.ExitCode != 0 {
+		fmt.Fprintf(os.Stderr, "error plotting graph in %s: exit status %d\n", dir, res.ExitCode)
 	}
 }
 
 func gatherValues(baseVal string) {
-	_, sigCh := process.Start("./capture.sh", []string{baseVal}, "../00-capture")
+	p, err := exec.LookPath("python3")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not lookup python3 path: %s\n", err)
+		os.Exit(1)
+	}
+	out, err := os.OpenFile("../01-plot/meter.csv", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0640)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open log file: %s\n", err)
+		os.Exit(1)
+	}
+	_, sigCh := process.Start(p, []string{"capture.py", baseVal}, "../00-capture", out)
 	res := <-sigCh
 	if res.Err != nil {
 		fmt.Fprintf(os.Stderr, "error gathering values: %s\n", res.Err)
@@ -64,11 +42,25 @@ func gatherValues(baseVal string) {
 	}
 }
 
+func dispatchPlotters() {
+	ticker := time.Tick(60 * time.Second)
+	for {
+		if err := filepath.Walk("../01-plot", func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				go plot(path)
+			}
+			return nil
+		}); err != nil {
+			fmt.Fprintf(os.Stderr, "error walking dir: %s", err)
+		}
+		<-ticker
+	}
+}
+
 func main() {
 	quitCh := make(chan struct{})
 
-	go plotValues()
-	go plotDaily()
+	go dispatchPlotters()
 	go gatherValues(os.Args[1])
 
 	<-quitCh
