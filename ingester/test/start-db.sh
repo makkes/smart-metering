8d40fcfe7eb1fc3a4c7ed128a57afd5cacb9316a
Max Jonas Werner <max@e13.dev>
2022-10-14 11:48:41 +0200

    add script for spinning up a test DB locally

---
+++ ingester/test/start-db.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+docker run --rm --name postgres -e POSTGRES_PASSWORD=max123 -p 5432:5432 -v "${SCRIPT_DIR}/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh postgres
