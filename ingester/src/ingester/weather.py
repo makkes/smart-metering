932c51f185e0d1bee1de524ef85219876bd80748
Max Jonas Werner <max@e13.dev>
2022-11-25 10:40:29 +0100

    take error values into account
    The DWD data uses the special value -999 to mark error values, e.g.
    when the data is not available due to sensors failing. In that case,
    the program now inserts a NULL value.

--- ingester/src/ingester/weather.py
+++ ingester/src/ingester/weather.py
@@ -13,6 +13,7 @@ def prepare_tables(db):
     migration = database.Migration("weather", [
         "CREATE TABLE temperatures(day DATE NOT NULL, min NUMERIC(3,1) NOT NULL, max NUMERIC(3,1) NOT NULL, avg NUMERIC(3,1) NOT NULL, PRIMARY KEY(day))",
         "ALTER TABLE temperatures ADD COLUMN precipitation NUMERIC(3,1)",
+        "ALTER TABLE temperatures ALTER COLUMN min DROP NOT NULL, ALTER COLUMN max DROP NOT NULL, ALTER COLUMN avg DROP NOT NULL",
     ])
     db.migrate(migration)
 
@@ -49,10 +50,16 @@ def insert_temps(data_file, db_conn):
             for row in r:
                 date = row[idxs["date"]].strip()
                 avg_t = float(row[idxs["avg"]].strip())
+                if avg_t == -999:
+                    avg_t = None
                 min_t = float(row[idxs["min"]].strip())
+                if min_t == -999:
+                    min_t = None
                 max_t = float(row[idxs["max"]].strip())
+                if max_t == -999:
+                    max_t = None
                 preci = float(row[idxs["precipitation"]].strip())
+                print(
+                    f"inserting ({date}, {min_t}, {avg_t}, {max_t}, {preci})")
                 cur.execute(
                     "INSERT INTO temperatures(day, min, max, avg, precipitation) VALUES(%(day)s, %(min)s, %(max)s, %(avg)s, %(precipitation)s) ON CONFLICT (day) DO UPDATE SET min = %(min)s, max = %(max)s, avg = %(avg)s, precipitation = %(precipitation)s", {"day": date, "min": min_t, "max": max_t, "avg": avg_t, "precipitation": preci})
-                print(
-                    f"inserted ({date}, {min_t}, {avg_t}, {max_t}, {preci})")
