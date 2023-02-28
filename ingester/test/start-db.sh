029cbab5ae84d14534f8be219c0ec7e8b759ac25
Max Jonas Werner <max@e13.dev>
2023-02-28 22:08:39 +0100

    start test DB in background

--- ingester/test/start-db.sh
+++ ingester/test/start-db.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-docker run --rm --name postgres -e POSTGRES_PASSWORD=max123 -p 5432:5432 -v "${SCRIPT_DIR}/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh postgres
+docker run -d --name postgres -e POSTGRES_PASSWORD=max123 -p 5432:5432 -v "${SCRIPT_DIR}/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh postgres
