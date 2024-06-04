8a8daa452ab7a2c645eb9d17be7c1dbaad862735
Max Jonas Werner <max.werner@associmates.eu>
2024-06-04 18:19:25 +0200

    Allow for providing 'docker' binary name to DB start script
    This way it's possible to use the script on machines that only have
    podman installed.

--- ingester/test/start-db.sh
+++ ingester/test/start-db.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+DOCKER_CMD="${DOCKER_CMD:-docker}"
 
-docker run -d --name postgres -e POSTGRES_PASSWORD=max123 -p 5432:5432 -v "${SCRIPT_DIR}/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh postgres
+$DOCKER_CMD run -d --name postgres -e POSTGRES_PASSWORD=max123 -p 5432:5432 -v "${SCRIPT_DIR}/init-db.sh":/docker-entrypoint-initdb.d/init-db.sh postgres
