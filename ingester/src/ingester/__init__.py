90b837acbbabc36aaec1ad173f2cbbdb29e58a9a
Max Jonas Werner <max@associmates.eu>
2024-12-29 11:26:51 +0100

    Fix and improve data ingestion
    the `ingest` function inserted readings with the wrong date because it
    always appended `CEST` for the timezone which caused PostgreSQL (or
    Psycopg2?) to interpret the time as UTC+2 regardless of the actual
    date (in winter time, i.e. CET, it is actually UTC+1).
    
    Now with the help of pytz, the correct time zone information is passed
    to PostgreSQL.
    
    The `readings` command now wraps all SQL statements in a single
    transaction by passing `autocommit=False` to the `connect` method.
    This changes the behaviour in a major way in that a single failure in
    any SQL statement will cause no changes to the DB. I find this a much
    safer way to handle errors and leave the DB in a consistent state.
    
    Additionally, this commit changes `ingest` in the following ways,
    making it run much faster than before:
    
    1. It only inserts values for dates that aren't stored in the `gas`
       table, yet by querying the most recent entry and skipping over all
       lines in the input CSV that are older (or equal) to that entry's
       date.
    2. It to use a multi-value insert statement which makes the function
       perform much faster than when it used a single insert statement per
       row.

--- ingester/src/ingester/__init__.py
+++ ingester/src/ingester/__init__.py
@@ -1,31 +1,50 @@
 import csv
+from datetime import datetime, timezone
+import logging
 import psycopg2
+from psycopg2.extras import execute_values
+import pytz
+
 
 def prepare_tables(conn):
     tables = {
-            "gas": "CREATE TABLE gas(dt TIMESTAMP WITH TIME ZONE NOT NULL, reading NUMERIC(7,2) NOT NULL, PRIMARY KEY(dt))",
-            "gas_per_day": "CREATE TABLE gas_per_day(day DATE NOT NULL, consumption NUMERIC(7,2) NOT NULL, PRIMARY KEY(day))"
-            }
+        "gas": "CREATE TABLE gas(dt TIMESTAMP WITH TIME ZONE NOT NULL, reading NUMERIC(7,2) NOT NULL, PRIMARY KEY(dt))",
+        "gas_per_day": "CREATE TABLE gas_per_day(day DATE NOT NULL, consumption NUMERIC(7,2) NOT NULL, PRIMARY KEY(day))"
+    }
     with conn.cursor() as cur:
         for name, ddl in tables.items():
-            cur.execute("SELECT COUNT(*) FROM information_schema.tables WHERE table_name = %s", (name,))
+            cur.execute(
+                "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = %s", (name,))
             res = cur.fetchone()
             if res[0] != 1:
                 cur.execute(ddl)
 
+
 def ingest(f, conn):
-    cur = conn.cursor()
-    with open(f,newline='') as c:
-        rdr = csv.reader(c, delimiter=',')
-        next(rdr) # discard title row
-        for row in rdr:
-            try:
-                row[0] += " CEST"
-                cur.execute("INSERT INTO gas(dt, reading) VALUES(%s, %s)", (row[0], row[1]))
-                print("inserted reading ({}, {})".format(row[0], row[1]))
-            except psycopg2.errors.UniqueViolation:
-                pass
-    cur.close()
+    with conn.cursor() as cur:
+        start_ts = None
+        # fetch most recent day from DB
+        cur.execute("SELECT dt FROM gas ORDER BY dt DESC LIMIT 1")
+        res = cur.fetchone()
+        if res != None:
+            start_ts = res[0]
+
+        with open(f, newline='') as c:
+            rdr = csv.reader(c, delimiter=',')
+            next(rdr)  # discard title row
+            inserts = []
+            for row in rdr:
+                dt = pytz.timezone("CET").localize(datetime.strptime(row[0], "%m/%d/%y %H:%M:%S"))
+                if start_ts is not None and dt <= start_ts:
+                    continue
+                inserts.append((dt.strftime("%Y-%m-%d %H:%M:%S%z"), row[1]))
+        try:
+            logging.info("inserting %s %s", len(inserts), "row" if len(inserts) == 1 else "rows")
+            execute_values(
+                cur, "INSERT INTO gas(dt, reading) VALUES %s ON CONFLICT(dt) DO NOTHING", inserts)
+        except Exception as e:
+            logging.info("error inserting data: %s", e)
+
 
 def update_per_day_consumption(conn):
     cur = conn.cursor()
@@ -55,5 +74,6 @@ def update_per_day_consumption(conn):
             pdc = [single_reading[1], single_reading[1], 0]
         per_day_consumption[day] = pdc
     for day, vals in per_day_consumption.items():
-        cur.execute("INSERT INTO gas_per_day(day, consumption) VALUES(%s, %s) ON CONFLICT (day) DO UPDATE SET consumption = %s", (day, vals[2], vals[2]))
-        print("inserted/updated daily consumption for {}".format(day))
+        cur.execute(
+            "INSERT INTO gas_per_day(day, consumption) VALUES(%s, %s) ON CONFLICT (day) DO UPDATE SET consumption = %s", (day, vals[2], vals[2]))
+        logging.info("inserted/updated daily consumption for %s", day)
